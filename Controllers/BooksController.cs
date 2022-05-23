891001592eaca6e4e56f0203bed0a6a03e430545
Dennisgreen7 <denis4167@gmail.com>
2022-05-23 14:41:23 +0300

    first commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(5)}")]
+        public string GetBooks(int id)
+        {
+            return "Book Id (INT - min 5): "+id;
+        }
+        [Route("{id:regex(book(boom|zoom))}")]
+        public string GetBooksRegex(string id)
+        {
+            return "Book Id (Regex): " + id;
+        }
+    }
+}
